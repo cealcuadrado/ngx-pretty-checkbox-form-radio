b20e1eea0fbec9899a2f3ff3ec909a74541075ec
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-09 12:04:15 -0400

    Cambio de valor en el formulario, valor inicial, creado método de submit

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PrettyCheckBoxChange, PrettyRadioChange } from 'ngx-pretty-checkbox';
 
 @Component({
   selector: 'app-layout',
@@ -8,14 +10,34 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
 
   numbers: any[] = [
-    { id: 0, name: 'Cero', value: 'Cero' },
-    { id: 1, name: 'Uno', value: 'Uno' },
-    { id: 2, name: 'Dos', value: 'Dos' },
-    { id: 3, name: 'Tres', value: 'Tres'}
+    { id: 0, name: 'Cero', value: 0 },
+    { id: 1, name: 'Uno', value: 1 },
+    { id: 2, name: 'Dos', value: 2 },
+    { id: 3, name: 'Tres', value: 3}
   ];
-  constructor() { }
+
+  radioForm: FormGroup;
+  initialValue = 0;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.radioForm = this.fb.group({
+      number: this.fb.control(0)
+    });
   }
 
+  onSelectRadio(event: PrettyRadioChange) {
+    this.radioForm.value.number = event.value;
+  }
+
+  isInitialValue(value: number): boolean {
+    return value === 0;
+  }
+
+  submitForm(): void {
+    console.log(this.radioForm.value);
+  }
 }
